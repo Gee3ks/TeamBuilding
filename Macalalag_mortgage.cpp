c0de6dba54279bc355339cd95fcd7ff8ee7397c5
Gee3ks <145691689+Gee3ks@users.noreply.github.com>
2023-12-06 21:19:52 +0800

    Update Macalalag_mortgage.cpp

--- Macalalag_mortgage.cpp
+++ Macalalag_mortgage.cpp
@@ -1,63 +1,79 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
 using namespace std;
 
-const double LOAN_RATE = 0.03;
-const double INTEREST_RATE = 0.06;
-const double TAX_RATE = 0.35;
-
-bool continue_running() {
-  string repeat("");
-  while (repeat != "y" && repeat != "n") {
-    cout << "Do you want to try again?(y,n) ";
-    cin >> repeat;
-  }
-  return repeat == "y";
+double calculate_mortgage_cost(double loan_balance, double interest_rate)
+{
+    double interest_payment = interest_rate * loan_balance;
+    double mortgage_cost = 0.03 * loan_balance + interest_payment;
+    return mortgage_cost;
 }
 
-int get_value(string message) {
-  int value;
-  do {
-    cout << message;
-    cin >> value;
-  } while (value < 1);
-  return value;
+double calculate_tax_savings(double interestPayment)
+{
+    double tax_rate = 0.35;
+    double tax_savings = tax_rate * interestPayment;
+    return tax_savings;
 }
 
-double calc_initial_loan(int house_price, int down_payment) {
-  return house_price - down_payment;
+double get_positive_value(const string &prompt_message)
+{
+    double num_value = 0;
+    do
+    {
+        cout << prompt_message;
+        if (cin >> num_value && num_value > 0.0)
+        {
+            break;
+        }
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        continue;
+    } while (true);
+    return num_value;
 }
 
-double calc_annual_tax(double annual_mortgage, double tax_savings) {
-  return annual_mortgage - tax_savings;
+bool continue_running()
+{
+    string response = "";
+    while (response != "y" && response != "n")
+    {
+        cout << "Do you want to continue? [y/n]: ";
+        cin >> response;
+    }
+    return response == "y";
 }
 
-double calc_annual_mortgage(double initial_loan_balance) {
-  return (LOAN_RATE * initial_loan_balance) + (INTEREST_RATE * initial_loan_balance);
-}
+int main()
+{
 
-void print_value() {
-  int house_price = get_value("Enter the price of the house: $");
-  int downpayment = get_value("Enter the downpayment: $");
-  double initial_loan_balance = calc_initial_loan(house_price, downpayment);
+    do
+    {
+        const double interest_rate = 0.06;
+        cout << fixed << setprecision(2);
+        double price = get_positive_value("Enter house price: $");
+        double down_payment = get_positive_value("Enter downpayment amount: $");
 
-  // Initialize total annual mortgage cost
-  double total_annual_mortgage_cost = 0;
+        double loan_balance = price - down_payment;
+        int years = 10;
 
-  for (int year = 1; year <= 10; year++) {
-    double annual_mortgage = calc_annual_mortgage(initial_loan_balance);
-    double tax_savings = TAX_RATE * (INTEREST_RATE * initial_loan_balance);
+        for (int year = 1; year <= years; ++year)
+        {
+            double interest_payment = interest_rate * loan_balance;
+            double mortgage_cost = calculate_mortgage_cost(loan_balance, interest_rate);
+            double tax_savings = calculate_tax_savings(interest_payment);
+            double after_tax_cost = mortgage_cost - tax_savings;
 
-    // Accumulate annual mortgage cost
-    total_annual_mortgage_cost += calc_annual_tax(annual_mortgage, tax_savings);
-  }
+            loan_balance -= mortgage_cost; // Update loanBalance
 
-  cout << "Total annual mortgage cost after 10 years: $" << total_annual_mortgage_cost << endl;
-}
+            cout << "Year " << year << ":" << endl;
+            cout << "  Mortgage Cost: $" << mortgage_cost << endl;
+            cout << "  Tax Savings: $" << tax_savings << endl;
+            cout << "  After-Tax Cost: $" << after_tax_cost << endl;
+        }
+    } while (continue_running());
+    cout << "Program exiting." << endl;
 
-int main(void) {
-  do {
-    print_value();
-  } while (continue_running());
-  return 0;
+    return 0;
 }
